3bf36c93a883c3d882c6447a7f2cc42f5c316d70
SHARJIDH <sharjidh2003@gmail.com>
2024-05-28 16:43:41 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import './App.css'
 import { motion } from 'framer-motion'
 import Slide from './components/slide/Slide'
 import Upper from "./components/upper/Upper";
-// import Grid from "./components/grid/Grid";
+
 
 function App() {
   return (
     <motion.div>
       <Upper />
       <Slide />
-      {/* <Grid /> */}
     </motion.div>
   )
 }
